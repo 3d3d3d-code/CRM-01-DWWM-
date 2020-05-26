614782e93e5e5f0119c64d551240efcbc04cc553
3d3d3d-code <reda.hamza8@orange.fr>
2020-05-26 16:59:27 +0200

    commit employes before correction

--- JAVASCRIPT/00_Exercices/02_employed.js
+++ JAVASCRIPT/00_Exercices/02_employed.js
@@ -6,58 +6,73 @@ class Employee {
         this.id = id;
         this.lastname = lastname;
         this.firstname = firstname;
-        this.email = email;
+        this.email = (lastname.substring(0, 1) + firstname.substring(0, 1)).toLowerCase();
         this.role = role;
         this.salary = salary;
         this.hiredate = hiredate;
     }
-       
-    function (getEmail){
-        var email = lastname.substring(0,1) + firstname.substring(0,1);
-     }
-    
+
+
+
+    function(getMonthSalary) {
+
+        return Math.round((this.salary / 12) * 0.75);
+
+    }
+
+    function(getSeniorty) {
+
+
+        var seniority = hiredate - date;
+        console.log(seniority);
+
+
+    }
+    function(getLittleSalary) {
+
+        return Math.min(firstname.salary);
+
+    }
+
+
+
+
 
 }
 
 
-        var employee1 = new Employee(1, 'Doe', 'John', '', 'manager', 82000, new Date('2020-12-28'));
-        console.log(employee1);
+var employee1 = new Employee(1, 'Doe', 'John', '', 'manager', 82000, new Date('2020-12-28'));
 
+console.log(employee1);
+var employees = [employee1];
+console.log();
 
-    
+var employee2 = new Employee(2, 'Jean', 'Passe', '', 'winner', 95000, new Date('2021-10-17'));
+employees.push(employee2);
+console.log(employee2);
+var employee3 = new Employee(3, 'Punchy', 'Line', '', 'faineant', 12000, new Date('2020-12-28'));
+employees.push(employee3);
 
-var employees =[ new Employee(2, 'Jean', 'Passe','', 'dessinateur', 95000, new Date('2021-10-17')),
-new Employee(3, 'Punchy', 'Line', '','manager', 82000, new Date('2020-12-28')),
-new Employee(4, 'Doe', 'John', '','manager', 82000, new Date('2020-12-28')),
-new Employee(5, 'What', 'John','', 'manager', 82000, new Date('2020-12-28')),
+var employee4 = new Employee(4, 'Pro', 'Fitter', '', 'manager', 56000, new Date('2020-12-28'));
+employees.push(employee4);
+var employee5 = new Employee(5, 'What', 'Happened', '', 'skieur', 87000, new Date('2020-12-28'));
+employees.push(employee5);
+for (var i = 0; i < employees.length; i++) {
+    console.log("Il y a " + employees.length + " employé(e)s.");
+}
+console.log();
+for (var i = 0; i < employees.length; i++) {
 
+    console.log('les employes se nomment :', employees[i].firstname);
+  //  console.log('le salaire le plus bas est celui de :', Math.min(salary[i].firstname));
+}
 
-]
-console.log(employees); 
-
-
-
-const employees = [employee1,employee2,employee3,employee4,employee5];
-console.log("Il y a " + employees.length + " employé(e)s.");
 console.log(employees);
 
 
 
 
 
-         
-
-
-
-
-
-        
-        /*function getMonthSalary() {
-
-            return Math.round((this.salary / 12)*0.75);
-        
-        }
-        getMonthSalary(employee1);*/
 
 
 
@@ -66,33 +81,32 @@ console.log(employees);
 
 
 
-    
-
-  
 
 
-/*
-console.log (getMonthSalary.employee1);
-
-/*function getSeniorty() {
 
 
-    var seniority = hiredate - date;
-    console.log(seniority());
 
 
-}
 
 
-/*function getBestSalary(){
-
-var bestSalary = salary.length;
-
-}
-Console.log(salary);
 
 
-//*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
