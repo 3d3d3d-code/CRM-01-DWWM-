203120f3e952b9c3c41763111d565a0bd8081052
3d3d3d-code <reda.hamza8@orange.fr>
2020-05-26 14:31:09 +0200

    commit avancee exo employe

--- JAVASCRIPT/00_Exercices/02_employed.js
+++ JAVASCRIPT/00_Exercices/02_employed.js
@@ -1,18 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Employee {
 
 
@@ -25,41 +11,70 @@ class Employee {
         this.salary = salary;
         this.hiredate = hiredate;
     }
+       
+    function (getEmail){
+        var email = lastname.substring(0,1) + firstname.substring(0,1);
+     }
+    
+
 }
 
 
-var employee1 = new Employee(1, 'Doe', 'John', '', 'manager', 82000, new Date('2020-12-28'));
-console.log(employee1);
+        var employee1 = new Employee(1, 'Doe', 'John', '', 'manager', 82000, new Date('2020-12-28'));
+        console.log(employee1);
 
 
-const employees = [employee1];
+    
+
+var employees =[ new Employee(2, 'Jean', 'Passe','', 'dessinateur', 95000, new Date('2021-10-17')),
+new Employee(3, 'Punchy', 'Line', '','manager', 82000, new Date('2020-12-28')),
+new Employee(4, 'Doe', 'John', '','manager', 82000, new Date('2020-12-28')),
+new Employee(5, 'What', 'John','', 'manager', 82000, new Date('2020-12-28')),
+
+
+]
+console.log(employees); 
 
 
 
-var employee2 = new Employee(2, 'Jean', 'Passe','Doe@aol.com', 'dessinateur', 95000, new Date('2021-10-17'));
-var employee3 = new Employee(3, 'Punchy', 'Line', 'Doe@aol.com','manager', 82000, new Date('2020-12-28'));
-var employee4 = new Employee(4, 'Doe', 'John', 'Doe@aol.com','manager', 82000, new Date('2020-12-28'));
-var employee4 = new Employee(5, 'What', 'John','Doe@aol.com', 'manager', 82000, new Date('2020-12-28'));
-
+const employees = [employee1,employee2,employee3,employee4,employee5];
 console.log("Il y a " + employees.length + " employé(e)s.");
-
-/*function getEmail(){
-    var email = browserType.slice(0,3);
-    console.log(email)
+console.log(employees);
 
 
+
+
+
+         
+
+
+
+
+
+        
+        /*function getMonthSalary() {
+
+            return Math.round((this.salary / 12)*0.75);
+        
+        }
+        getMonthSalary(employee1);*/
+
+
+
+
+
+
+
+
+    
 
   
 
 
-/*function getMonthSalary() {
+/*
+console.log (getMonthSalary.employee1);
 
-    var MonthSalary = salary / (12 * 0.75);
-    console.log(MonthSalary);
-
-}
-
-function getSeniorty() {
+/*function getSeniorty() {
 
 
     var seniority = hiredate - date;
